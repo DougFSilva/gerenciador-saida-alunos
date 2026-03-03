aa47d6bfcf379cd601aaebd16a819c9aca7031a1
DougFSilva <doug.ferreira.dfs@gmail.com>
2026-03-03 15:43:57 -0300

    Primeiro commit

---
+++ src/main/java/com/dougfsilva/controlesaidaescolar/ControlesaidaescolarApplication.java
@@ -0,0 +1,13 @@
+package com.dougfsilva.controlesaidaescolar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlesaidaescolarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlesaidaescolarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dougfsilva/controlesaidaescolar/ControlesaidaescolarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dougfsilva.controlesaidaescolar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlesaidaescolarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
