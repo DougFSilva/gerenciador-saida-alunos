47fc1ea21423303355d2449610a46d8c7502da66
DougFSilva <doug.ferreira.dfs@gmail.com>
2026-03-04 10:30:49 -0300

    Concluido service AtivarDesativarTurmaService

--- src/main/java/com/dougfsilva/controlesaidaescolar/service/turma/AtivarDesativarTurmaService.java
+++ src/main/java/com/dougfsilva/controlesaidaescolar/service/turma/AtivarDesativarTurmaService.java
@@ -1,5 +1,37 @@
 package com.dougfsilva.controlesaidaescolar.service.turma;
 
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Service;
+
+import com.dougfsilva.controlesaidaescolar.model.Turma;
+import com.dougfsilva.controlesaidaescolar.repository.TurmaRepository;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
 public class AtivarDesativarTurmaService {
 
+	private final TurmaRepository repository;
+	
+	@Transactional
+	@PreAuthorize("hasRole('ADMIN')")
+	public void ativarTurma(Long id) {
+		Turma turma = repository.findByIdOrElseThrow(id);
+		turma.setAtiva(true);
+		Turma turmaAtivada = repository.save(turma);
+		log.info("Turma ativada com sucesso! ID: {}", turmaAtivada.getId());
+	}
+	
+	@Transactional
+	@PreAuthorize("hasRole('ADMIN')")
+	public void desativarTurma(Long id) {
+		Turma turma = repository.findByIdOrElseThrow(id);
+		turma.setAtiva(false);
+		Turma turmaAtivada = repository.save(turma);
+		log.info("Turma desativada com sucesso! ID: {}", turmaAtivada.getId());
+	}
 }
